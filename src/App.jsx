13eeb83e6da63c5d6fe55ba0b176f3207a2d4a50
roryyu <rory.yu@kupotech.com>
2023-06-07 09:08:43 +0800

    modify menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import demos from "./demos";
 
 export default function App() {
-  const [currentCompoment, setCurrentCompoment] = useState(demos[demos.length - 1].compoment);
-  const handleClick = (compoment) => {
-    setCurrentCompoment(compoment);
+  const lastChapter = demos[demos.length - 1];
+  const lastChildren = lastChapter.children[lastChapter.children.length - 1];
+  const [currentSection, setCurrentSection] = useState(lastChildren);
+  const handleClick = (section) => {
+    setCurrentSection(section);
   };
 
   return (
@@ -12,13 +14,24 @@ export default function App() {
       <aside className="menu">
         <ul>
           {demos.map((item) => (
-            <li style={{ cursor: "pointer" }} key={item.title} onClick={() => handleClick(item.compoment)}>
-              {item.title}
+            <li key={item.chapter}>
+              {item.chapter}
+              <ol style={{ listStyle: "auto", marginLeft: "50px" }}>
+                {item.children.map((children) => (
+                  <li
+                    key={children.title}
+                    className={currentSection.title === children.title ? "active" : ""}
+                    onClick={() => handleClick(children)}
+                  >
+                    {children.title}
+                  </li>
+                ))}
+              </ol>
             </li>
           ))}
         </ul>
       </aside>
-      <main>{currentCompoment}</main>
+      <main>{currentSection.compoment}</main>
     </section>
   );
 }
--- src/demos.jsx
+++ src/demos.jsx
@@ -1,50 +1,60 @@
-import Game from "./views/game";
-import FilterableProductTable from "./views/filterable-product-table";
-import Gallery from "./view/1-first-component/Gallery";
-import TodoList from "./views/2-writing-markup-with-jsx/TodoList";
-import Card from "./views/3-mixing JavaScript and JSX/Card";
-import Profile from "./views/4-passing-props-to-component/Profile";
-import PackingList from "./views/5-conditional-rendering/PackingList";
-import List from "./views/6-rendering-lists/List";
-import TeaSet from "./views/7-keeping-components-pure/TeaSet";
+import Game from "./views/1-quick-start/game";
+import FilterableProductTable from "./views/1-quick-start/filterable-product-table";
+import Gallery from "./views/2-describing-the-ui/1-first-component/Gallery";
+import TodoList from "./views/2-describing-the-ui/2-writing-markup-with-jsx/TodoList";
+import Card from "./views/2-describing-the-ui/3-mixing JavaScript and JSX/Card";
+import Profile from "./views/2-describing-the-ui/4-passing-props-to-component/Profile";
+import PackingList from "./views/2-describing-the-ui/5-conditional-rendering/PackingList";
+import List from "./views/2-describing-the-ui/6-rendering-lists/List";
+import TeaSet from "./views/2-describing-the-ui/7-keeping-components-pure/TeaSet";
 
 const demos = [
-    {
-      title: "tic-tac-toe",
-      compoment: <Game />,
-    },
-    {
-      title: "FilterableProductTable",
-      compoment: <FilterableProductTable />,
-    },
-    {
-      title: "my first component",
-      compoment: <Gallery />,
-    },
-    {
-      title: "JSX ",
-      compoment: <TodoList />,
-    },
-    {
-      title: "mixing JavaScript and JSX ",
-      compoment: <Card />,
-    },
-    {
-      title: "passing props to component ",
-      compoment: <Profile />,
-    },
-    {
-      title: "conditional rendering ",
-      compoment: <PackingList />,
-    },
-    {
-      title: "rendering lists",
-      compoment: <List />,
-    },
-    {
-      title: "keeping-components-pure",
-      compoment: <TeaSet />,
-    },
-  ];
+  {
+    chapter: "1-quick-start",
+    children: [
+      {
+        title: "tic-tac-toe",
+        compoment: <Game />,
+      },
+      {
+        title: "FilterableProductTable",
+        compoment: <FilterableProductTable />,
+      },
+    ],
+  },
+  {
+    chapter: "2-describing-the-ui",
+    children: [
+      {
+        title: "my first component",
+        compoment: <Gallery />,
+      },
+      {
+        title: "JSX ",
+        compoment: <TodoList />,
+      },
+      {
+        title: "mixing JavaScript and JSX ",
+        compoment: <Card />,
+      },
+      {
+        title: "passing props to component ",
+        compoment: <Profile />,
+      },
+      {
+        title: "conditional rendering ",
+        compoment: <PackingList />,
+      },
+      {
+        title: "rendering lists",
+        compoment: <List />,
+      },
+      {
+        title: "keeping-components-pure",
+        compoment: <TeaSet />,
+      },
+    ],
+  },
+];
 
-  export default demos;
+export default demos;

b2b3344e81798de1ca060c15f9c492490455a27f
roryyu <rory.yu@kupotech.com>
2023-07-10 08:34:15 +0800

    add comment

--- src/views/5-escape-hatches/3-synchronizing-with-effects/MyApp.jsx
+++ src/views/5-escape-hatches/3-synchronizing-with-effects/MyApp.jsx
@@ -30,9 +30,27 @@ export default function MyApp(){
 }
 
 /**
- * Effects run at the end of a commit after the screen updates
- * Effect 在组件更新后的 commit 结束时运行
  * 
- * React will call your cleanup function each time before the Effect runs again, and one final time when the component unmounts (gets removed).
- * Effect 的 cleanup函数会在每次 Effect 运行之前运行，以及在组件卸载（被移除）时运行一次。
+ * 1、Effect 在组件更新后的 commit 结束时运行
+ * （Effects run at the end of a commit after the screen updates）
+ * 
+ * 2、Effect 的 cleanup函数会在每次 Effect 运行之前运行，以及在组件卸载（被移除）时运行一次。
+ * （React will call your cleanup function each time before the Effect runs again, and one final time when the component unmounts (gets removed).）
+ * 
+ * 3、Effect 依赖项数组（Dependency Array）的作用
+ * （1）无依赖：每次组件更新时都会运行 Effect 
+ * useEffect(() => {
+ *   // This runs after every render
+ *  });
+ * 
+ * （2）依赖为空数组：只在组件挂载时运行 Effect
+ * useEffect(() => {
+ *  // This runs only on mount (when the component appears)
+ * }, []);
+ * 
+ * （3）有依赖：挂载和只有当依赖项发生变化时才会运行 Effect
+ * useEffect(() => {
+ * // This runs on mount *and also* if either a or b have changed since the last render
+ * }, [a, b]);
+ * 
  */
